c88033de72c3e61f6a83d0ecce682c58f6439de4
pmartinsdev <p6220064@brasilprev.com.br>
2024-03-17 12:57:14 -0300

    feat: adicionar hands-on, arquivo de exercicios de fixação dentro da aula04

---
+++ aula_04/hands-on.js
@@ -0,0 +1,30 @@
+// Exercícios de Funções
+
+// Exercício 1: Função de Saudação
+// Crie uma função chamada saudar que receba um nome como parâmetro e retorne a mensagem "Olá, [nome]!".
+// Exemplo: saudar("Paulo") deve retornar "Olá, Paulo!"
+
+// Exercício 2: Função de Soma
+// Crie uma função chamada somar que receba dois números como parâmetros e retorne a soma deles.
+// Exemplo: somar(3, 5) deve retornar 8
+
+// Exercício 3: Função de Verificação de Idade
+// Crie uma função chamada verificarMaioridade que receba uma idade como parâmetro e retorne "Maior de idade" se a idade for maior ou igual a 18, e "Menor de idade" caso contrário.
+// Exemplo: verificarMaioridade(20) deve retornar "Maior de idade"
+
+// Exercícios de Classes
+
+// Exercício 1: Classe Pessoa
+// Crie uma classe chamada Pessoa com um construtor que receba o nome e a idade da pessoa. Adicione um método chamado apresentar que retorne a mensagem "Meu nome é [nome] e tenho [idade] anos.".
+// Exemplo: 
+// const pessoa = new Pessoa("Paulo", 23);
+// pessoa.apresentar(); // deve retornar "Meu nome é Paulo e tenho 23 anos."
+
+// Exercício 2: Classe Contador
+// Crie uma classe chamada Contador com um construtor que inicie o contador em zero. Adicione métodos para incrementar (incrementar), decrementar (decrementar) e obter o valor atual do contador (getValor).
+// Exemplo: 
+// const contador = new Contador();
+// contador.incrementar();
+// contador.getValor(); // deve retornar 1
+// contador.decrementar();
+// contador.getValor(); // deve retornar 0
