ea099b34f487bd7e09bc013a0287d277d4a2c4df
pmartindev <martins20.tech@gmail.com>
2024-03-30 14:42:08 -0300

    feat: add  hands-on exercise

---
+++ aula_07/hands-on/script.js
@@ -0,0 +1,10 @@
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+
+addTaskBtn.addEventListener('click', () => {
+    // 1. Obter o valor do input
+    // 2. Criar um novo elemento <li> e adicionar o texto da tarefa
+    // 3. Adicionar o novo elemento <li> à lista de tarefas (taskList)
+    // 4. Limpar o input
+});
