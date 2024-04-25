aa93ed1af4570ef463263cbc4c5513f1d0d2b8cf
pmartinsdev <martins20.tech@gmail.com>
2024-04-25 19:44:20 -0300

    feat: clear CRA template and create greetings components in 2 ways as class and function

---
+++ modulo_02/aula_05/src/components/greetings/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export class GreetingsClass extends React.Component {
+  render() {
+    return (
+      <h1>Hello From Greetings Class</h1>
+    )
+  }
+}
+
+export function GreetingsFunction() {
+  return (
+    <h1>Hello From Greetings Function</h1>
+  )
+}
